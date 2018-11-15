f02b03ef18461eed85257d639c012c7628f6f496
skatta3 <skatta3@ncsu.edu>
2018-11-15 02:25:06 -0500

    check

---
+++ scripts/parser.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+fname=$1
+sed 's/\r$//' "$fname"    |\
+grep -v -- "-->"          |\
+grep -v "^$"              |\
+grep -E -v "^[0-9]+$"     |\
+sed 's/WEBVTT//'          |\
+tr '\n' ' '               |\
+tr -s ' '                 |\
+tr -d '\t'                |\
+sed 's/\\/\\\\/g'         |\
+sed 's/"/\\"/g'
