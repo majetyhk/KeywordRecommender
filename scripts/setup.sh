5a481af28f0d545aec5fd6fc46280b669e7a713e
skatta3 <skatta3@ncsu.edu>
2018-11-15 02:28:35 -0500

    script files core logic

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+# install pip 
+# https://www.saltycrane.com/blog/2010/02/how-install-pip-ubuntu/
+apt-get install python-pip python-dev build-essential
+pip install --upgrade pip
+# update youtube-dl
+sudo -H pip install --upgrade youtube-dl
