d13c9f8f6f067372de0a58322ec7dd86d6bd9393
skatta3 <skatta3@ncsu.edu>
2018-11-16 17:04:39 -0500

    Fixed: standardize scripts

--- scripts/extractor.sh
+++ scripts/extractor.sh
@@ -3,26 +3,31 @@
 
 if [ $# -eq 0 ]; then
     echo "No arguments supplied supplied. Usage: ./scrtpt.sh video-id"
+    exit 0
 fi
 
-
+mkdir -p "$HOME/subtitles/"
+mkdir -p "$HOME/text/"
+cp "$PWD/parser.sh" "$HOME/subtitles/."
 #method 1, extract subtitles uploaded by provider - example - uuatZO76MgQ
-youtube-dl --write-sub --sub-lang en --skip-download https://www.youtube.com/watch?v="$1" -o "$1"
+youtube-dl --write-sub --sub-lang en --skip-download https://www.youtube.com/watch?v="$1" -o "$HOME/subtitles/$1"
 
 
 #if [ ! -f /"$PWD"/"$1*" ]; then
-if ls /"$PWD"/"$1"* 1> /dev/null 2>&1; then
-    ./parser.sh "$1".en.vtt > "$1".txt
+if ls "$HOME/subtitles/$1"* 1> /dev/null 2>&1; then
+    echo "downloaded from provider uploaded SRT, extract it"
+    ./parser.sh "$HOME/subtitles/$1".en.vtt > "$HOME/text/$1".txt
     exit 0
 
 else
     echo "Fall back - Download automatic subtitles form youtube"
     # method 2, extrac automatic subtitles   example - 8f3ijY0MJBk
-    youtube-dl --write-auto-sub --skip-download https://www.youtube.com/watch?v="$1" -o "$1"
+    youtube-dl --write-auto-sub --skip-download https://www.youtube.com/watch?v="$1" -o "$HOME/subtitles/$1"
 fi
 
-if ls /"$PWD"/"$1"* 1> /dev/null 2>&1; then
-    ./new_parser.sh "$1".en.vtt > "$1".txt
+if ls "$HOME/subtitles/$1"* 1> /dev/null 2>&1; then
+    echo "prase the auto downloaded str files"
+    ./new_parser.sh "$HOME/subtitles/$1".en.vtt > "$HOME/text/$1".txt
 else
     echo "Fall back - mp3 download and convert text from there"
     #youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v="$1" -o "$1.mp3"
