a53e0223e763190a047750dc8abdb3ef2056abf0
skatta3 <skatta3@ncsu.edu>
2018-11-16 19:55:21 -0500

    Wrapper scripts

---
+++ scripts/wrapper_setup.sh
@@ -0,0 +1,8 @@
+
+pip install --upgrade google-api-python-client
+
+pip install --upgrade google-auth google-auth-oauthlib google-auth-httplib2
+
+pip install --upgrade flask
+
+pip install --upgrade requests
