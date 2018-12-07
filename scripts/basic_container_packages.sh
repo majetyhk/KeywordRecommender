ed11361717e37604590ca21075bb1cff2cef901d
skatta3 <skatta3@ncsu.edu>
2018-12-06 19:49:39 -0500

    Create basic_container_packages.sh

---
+++ scripts/basic_container_packages.sh
@@ -0,0 +1,10 @@
+
+
+apt-get update
+apt-get -y -qq install python3
+apt-get -y -qq install python3-pip
+apt-get -y -qq install git
+apt-get -y -qq install iputils-ping
+apt-get -y -qq install iproute2
+apt-get -y -qq install vim
+apt-get -y -qq install dnsutils
