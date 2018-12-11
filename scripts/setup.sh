11175ccac1f7b107d475484cbd14753a6f91ec44
skatta3 <skatta3@ncsu.edu>
2018-12-10 19:17:05 -0500

    Feature update developer commands and few slight changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,9 @@ curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-
 chmod a+rx /usr/local/bin/youtube-dl
 # install pip 
 # https://www.saltycrane.com/blog/2010/02/how-install-pip-ubuntu/
-apt-get install python-pip python-dev build-essential -y
-pip install --upgrade pip 
+apt-get install python3 python3-pip python-dev build-essential -y
+#pip install --upgrade pip 
 # update youtube-dl
 sudo -H pip install --upgrade youtube-dl
+
+bash "$HOME"/Keyword_Recommender/scripts/wrapper_setup.sh
